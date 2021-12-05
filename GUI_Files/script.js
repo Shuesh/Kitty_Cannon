0982a27810d280e0fdb24c0e9b81fd1309049b76
Shuesh <holtz185@umn.edu>
2021-12-05 14:48:18 -0600

    Kitty Cannon - update structure

---
+++ GUI_Files/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("keydown", function (event){
+    if (event.defaultPrevented){
+        return;
+    }
+    switch (event.key){
+        case "ArrowUp":
+            
+            break;
+        case "ArrowLeft":
+
+            break;
+        case "ArrowRight":
+
+            break;
+        case "ArrowDown":
+
+            break;
+        case "Space":
+        
+            break;
+    }
+    event.preventDefault();
+}, true);
