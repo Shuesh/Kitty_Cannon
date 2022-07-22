1318ebda56fff114761e53b52b122f2d302ef210
Josh <holtz185@umn.edu>
2022-07-21 22:26:30 -0500

    Updated html, added svgs and refactored css

--- GUI_Files/script.js
+++ GUI_Files/script.js
@@ -4,20 +4,20 @@ window.addEventListener("keydown", function (event){
     }
     switch (event.key){
         case "ArrowUp":
-            
+            console.log(event.key);
             break;
         case "ArrowLeft":
-
+            console.log(event.key);
             break;
         case "ArrowRight":
-
+            console.log(event.key);
             break;
         case "ArrowDown":
-
+            console.log(event.key);
             break;
-        case "Space":
-        
+        case " ":
+            console.log(event.key);
             break;
     }
-    event.preventDefault();
+    // event.preventDefault();
 }, true);
